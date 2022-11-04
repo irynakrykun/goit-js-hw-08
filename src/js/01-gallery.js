fd732ae3c41809689100dd1c80c73f8c0656aac5
irynakrykun <ira_franyk@ukr.net>
2022-11-04 12:10:14 +0200

    add libeary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+
 
 console.log(galleryItems);
