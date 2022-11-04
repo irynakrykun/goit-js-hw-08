cb0e9805b40f89aea1b4d0ad6e8d307adbbfb307
irynakrykun <ira_franyk@ukr.net>
2022-11-04 21:51:59 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,22 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
-
 
 console.log(galleryItems);
+
+const GalleryAll = document.querySelector('.gallery');
+const GalleryItems = CreateGalleryItems(galleryItems);
+GalleryAll.insertAdjacentHTML('beforeend', GalleryItems)
+
+function CreateGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+      return ` <div class="gallery__box">
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" loading ='lazy' src="${ preview}" alt="${description}" />
+</a>
+</div> `;
+    }).join('')
+} 
+const lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt',captionDelay:'250' });
