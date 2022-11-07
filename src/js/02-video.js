acfbe7c09cbfdcc8e93a04d006d9c3879880aeab
irynakrykun <ira_franyk@ukr.net>
2022-11-07 18:31:42 +0200

    chek

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,28 +4,20 @@ console.log(Player);
 
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-const data = {
-  duration: 61.857,
-  percent: 0.485,
-  seconds: 30,
-};
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
 player.on('timeupdate', throttle(onTimeupdate, 500));
  
 function onTimeupdate (data) {
     localStorage.setItem(LOCALSTORAGE_KEY,JSON.stringify(data.seconds))
 };
 
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY)).then((seconds) => {
-        console.log(seconds);
-}).catch((error) => {
-    console.log(error);    
-});
+const SavedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+if (SavedTime) {
+  player.setCurrentTime(SavedTime);
+};
+
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,40 @@
 import throttle from 'lodash.throttle';
 
 const formEL = document.querySelector(".feedback-form");
-const textareaEl = document.querySelector('textarea');
-const inputEl = document.querySelector('input');
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
-const object = localStorage.getItem(LOCALSTORAGE_KEY) || {};
-
+ 
 formEL.addEventListener('input', throttle(onFormInput, 500));
 formEL.addEventListener('submit', onFormSubmit);
 
+let object = localStorage.getItem(LOCALSTORAGE_KEY) || {};
 
+onUpdatePage();
 
 function onFormInput(e) {
+    object ? JSON.stringify(object) : {};
     object[e.target.name] = e.target.value
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(object));
-         
+       console.log(object[e.target.name])  
 };
+
+
 function onFormSubmit(e) {
     e.preventDefault();
     e.target.reset();
      localStorage.removeItem(LOCALSTORAGE_KEY);
-    console.log(object);
+   
 };
 
 function onUpdatePage() {
-    const saveValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    let saveValue = localStorage.getItem(LOCALSTORAGE_KEY);
     
-    if ( saveValue ) {
-        textareaEl.value = saveValue.message;
-        inputEl.value = saveValue.email;
+    if (saveValue) {
+        saveValue = JSON.parse(saveValue);
+        Object.entries(saveValue).forEach(([name, value]) => {
+            object[name] = value;
+            formEL.elements[name].value = value;
+        });
+        
     }
 };
-onUpdatePage()
