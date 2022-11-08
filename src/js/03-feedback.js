b08b87035aa4f2f00900ce4ea4062ee141cd0dfc
irynakrykun <ira_franyk@ukr.net>
2022-11-08 17:57:08 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,34 @@
+
+// import throttle from 'lodash.throttle';
+
+// const formEL = document.querySelector(".feedback-form");
+
+// const LOCALSTORAGE_KEY = "feedback-form-state";
+ 
+// formEL.addEventListener('input', throttle(onFormInput, 500));
+// formEL.addEventListener('submit', onFormSubmit);
+
+// const object = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+
+// onUpdatePage();
+
+// function onFormInput(e) {
+//     object[e.target.name] = e.target.value
+//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(object));
+// };
+
+// function onFormSubmit(e) {
+//     e.preventDefault();
+//     e.target.reset();
+//     localStorage.removeItem(LOCALSTORAGE_KEY);
+// };
+
+// function onUpdatePage() {    
+//     Object.entries(object).forEach(([key, value]) => {
+//             formEL[key].value = value
+//     })
+// };
+
 import throttle from 'lodash.throttle';
 
 const formEL = document.querySelector(".feedback-form");
@@ -7,34 +38,29 @@ const LOCALSTORAGE_KEY = "feedback-form-state";
 formEL.addEventListener('input', throttle(onFormInput, 500));
 formEL.addEventListener('submit', onFormSubmit);
 
-let object = localStorage.getItem(LOCALSTORAGE_KEY) || {};
-
 onUpdatePage();
 
 function onFormInput(e) {
-    object ? JSON.stringify(object) : {};
-    object[e.target.name] = e.target.value
+     let object = localStorage.getItem(LOCALSTORAGE_KEY);
+             object =  object ? JSON.parse(object):object = {} ;
+        object[e.target.name] = e.target.value
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(object));
-       console.log(object[e.target.name])  
+       console.log(object)  
+          
 };
-
 
 function onFormSubmit(e) {
     e.preventDefault();
     e.target.reset();
      localStorage.removeItem(LOCALSTORAGE_KEY);
-   
-};
-
+   };
 function onUpdatePage() {
     let saveValue = localStorage.getItem(LOCALSTORAGE_KEY);
     
     if (saveValue) {
         saveValue = JSON.parse(saveValue);
         Object.entries(saveValue).forEach(([name, value]) => {
-            object[name] = value;
-            formEL.elements[name].value = value;
+         formEL.elements[name].value = value;
         });
-        
-    }
+            }
 };
